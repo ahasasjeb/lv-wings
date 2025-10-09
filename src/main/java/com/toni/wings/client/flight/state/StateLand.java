f7f619272e7183a2da74eba6fa5991ce1a26dcae
lvjia <l1196444919@outlook.com>
2025-10-09 16:24:30 +0800

    恢复

--- src/main/java/com/toni/wings/client/flight/state/StateLand.java
+++ src/main/java/com/toni/wings/client/flight/state/StateLand.java
@@ -3,10 +3,8 @@ package com.toni.wings.client.flight.state;
 import com.toni.wings.client.flight.Animator;
 
 public final class StateLand extends State {
-    private static final int LAND_STATE_DELAY = 10;
-
     public StateLand() {
-        super(LAND_STATE_DELAY, Animator::beginLand);
+        super(Animator::beginLand);
     }
 
     @Override
