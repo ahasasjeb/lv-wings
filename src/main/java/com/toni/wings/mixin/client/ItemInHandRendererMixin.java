a44fac8666f76858e0d07571ee58eda2aca67472
lvjia <l1196444919@outlook.com>
2025-12-07 07:06:45 +0800

    feat: 添加空手渲染支持，优化玩家手臂渲染逻辑

--- src/main/java/com/toni/wings/mixin/client/ItemInHandRendererMixin.java
+++ src/main/java/com/toni/wings/mixin/client/ItemInHandRendererMixin.java
@@ -6,19 +6,30 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ItemInHandRenderer.class)
 abstract class ItemInHandRendererMixin {
     @Shadow
     private ItemStack mainHandItem;
 
-    @ModifyVariable(method = "renderArmWithItem(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("STORE"), ordinal = 0)
-    private boolean wings$renderEmptyOffhand(boolean original, AbstractClientPlayer player, float tickDelta, float pitch, InteractionHand hand, float swingProgress, ItemStack stack, float equipProgress, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
-        return WingsHooksClient.onCheckRenderEmptyHand(original, this.mainHandItem);
+    @Shadow
+    protected abstract void renderPlayerArm(PoseStack poseStack, MultiBufferSource buffer, int packedLight, float equipProgress, float swingProgress, HumanoidArm arm);
+
+    @Inject(method = "renderArmWithItem(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z", ordinal = 0, shift = At.Shift.AFTER),
+        locals = LocalCapture.CAPTURE_FAILHARD)
+    private void wings$renderEmptyOffhand(AbstractClientPlayer player, float tickDelta, float pitch, InteractionHand hand, float swingProgress, ItemStack stack, float equipProgress,
+                                           PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci, boolean isMainHand, HumanoidArm arm) {
+        if (!isMainHand && stack.isEmpty() && !player.isInvisible() && WingsHooksClient.onCheckRenderEmptyHand(false, this.mainHandItem)) {
+            this.renderPlayerArm(poseStack, buffer, packedLight, equipProgress, swingProgress, arm);
+        }
     }
 }
